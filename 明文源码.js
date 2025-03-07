c3a505c69c66a32175a11527eed16285ec52ee87
seahawkQQ <1257804943@qq.com>
2025-03-07 11:33:59 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 // 部署完成后在网址后面加上这个，只获取自建节点，/?token=xxoo
 
 
-const mytoken = 'f150f32a-2995-49e1-ab2a-d9278f74aaea'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
+const mytoken = '9c48087a-c86e-48c6-af82-4ea15e1c6aa8'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
 const tgbottoken ='6501178601:AAF2F5RYMjwM7DaEHF7SYvjrPZipk43xmfo'; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 const tgchatid ='1028572195'; //可以为空，或者@userinfobot中获取，/start
 
